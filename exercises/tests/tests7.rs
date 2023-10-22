a4db31364041c3c155c8c441c1bf67b97c99371a
NBZYB <124670477+NBZYB@users.noreply.github.com>
2023-10-22 12:31:50 +0000

    lalal

--- exercises/tests/tests7.rs
+++ exercises/tests/tests7.rs
@@ -34,9 +34,10 @@
 // Execute `rustlings hint tests7` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-fn main() {}
+
+fn main() { 
+}
 
 #[cfg(test)]
 mod tests {
@@ -53,3 +54,4 @@ mod tests {
         assert!(timestamp >= e && timestamp < e + 10);
     }
 }
+
